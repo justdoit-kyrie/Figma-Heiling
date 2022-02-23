0d4ce91fd3c046c49971232fbe5d16f6995e1812
justdoit-kyrie <ducndmse151198@gmail.com>
2022-02-23 20:42:57 +0700

    change

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 const toggle = document.querySelector(".header-toggle");
 const menu = document.querySelector(".header-menu");
+const items = document.querySelectorAll(".project-actions span");
+const images = document.querySelectorAll(".project-image");
 
 toggle.addEventListener("click", () => {
   menu.classList.add("active");
@@ -10,4 +12,19 @@ document.addEventListener("click", (e) => {
     return;
 
   menu.classList.remove("active");
+});
+
+items.forEach((item, index) => {
+  item.addEventListener("click", (e) => {
+    const prev = document.querySelector(".project-actions .active");
+    const prevImg = document.querySelector(".project-image.active");
+
+    // active button
+    prev.classList.remove("active");
+    e.target.classList.add("active");
+
+    // show current img
+    prevImg.classList.remove("active");
+    images[index].classList.add("active");
+  });
 });
